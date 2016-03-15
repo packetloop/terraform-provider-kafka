3607325123bae77b79e443a6b52a5353bd77fe36
Alexey Raga <alexey.raga@gmail.com>
2016-03-15 13:28:10 +1100

    Retention policies and cleanup strategy

--- kafka_client.go
+++ kafka_client.go
@@ -19,10 +19,36 @@ type KafkaManagingClient struct {
 type KafkaTopicInfo struct {
   PartitionsCount int
   ReplicationFactor int
+  CleanupPolicy string
+  RetentionBytes int64
+  RetentionMs int64
+}
+
+func appendConf(slice []string, name string, value string) []string {
+  return append(slice, "--config", name + "=" + value)
+}
+
+func(conf *KafkaTopicInfo) kafkaTopicConfigOpts() []string {
+  var parms = []string{}
+  if (conf.CleanupPolicy != "") { parms = appendConf(parms, "cleanup.policy", conf.CleanupPolicy ) }
+  if (conf.RetentionBytes > -1) { parms = appendConf(parms, "retention.bytes", strconv.FormatInt(conf.RetentionBytes, 10))}
+  if (conf.RetentionMs > -1)    { parms = appendConf(parms, "retention.ms", strconv.FormatInt(conf.RetentionMs, 10))}
+
+  return parms
 }
 
 func(info *KafkaTopicInfo) exists() bool {
   return info != nil && info.PartitionsCount > 0 && info.ReplicationFactor > 0
+}
+
+func newKafkaTopicInfo(partitions int, replicas int) *KafkaTopicInfo {
+  return &KafkaTopicInfo{
+    PartitionsCount:   partitions,
+    ReplicationFactor: replicas,
+    CleanupPolicy:     "",
+    RetentionBytes:    -1,
+    RetentionMs:       -1,
+  }
 }
 
 func (client *KafkaManagingClient) deleteTopic(name string) error {
@@ -46,20 +72,30 @@ func (client *KafkaManagingClient) deleteTopic(name string) error {
   return fmt.Errorf("Was not able to confirm that topic %s was marked for deletion. Something is wrong", name)
 }
 
-func (client *KafkaManagingClient) createTopic(name string, partitions int, replicas int) (*KafkaTopicInfo, error) {
-  cmd := exec.Command(
-    client.TopicScript,
+func (client *KafkaManagingClient) createTopic(name string, conf *KafkaTopicInfo) error {
+  var params = []string {
     "--zookeeper", client.Zookeeper,
     "--create", "--topic", name,
-    "--partitions", strconv.Itoa(partitions),
-    "--replication-factor", strconv.Itoa(replicas))
+    "--partitions", strconv.Itoa(conf.PartitionsCount),
+    "--replication-factor", strconv.Itoa(conf.ReplicationFactor),
+  }
+
+  confOpts := conf.kafkaTopicConfigOpts()
+  params = append(params, confOpts...)
+
+  log.Println("[DEBUG] Will execute %v", params)
+
+  cmd := exec.Command(
+    client.TopicScript,
+    params...
+    )
 
   out, err := cmd.Output()
 
   if err != nil {
     kafkaError := readError(string(out))
-    if (kafkaError != nil) { return nil, kafkaError }
-    return nil, err
+    if (kafkaError != nil) { return kafkaError }
+    return err
   }
 
   strOut := strings.TrimSpace(string(out))
@@ -67,14 +103,10 @@ func (client *KafkaManagingClient) createTopic(name string, partitions int, repl
   log.Println(strOut)
 
   if strOut == fmt.Sprintf("Created topic \"%s\".", name) {
-    res := &KafkaTopicInfo{
-      PartitionsCount: partitions,
-      ReplicationFactor: replicas,
-    }
-    return res, nil
+    return nil
   }
 
-  return nil, fmt.Errorf("Unable to parse results from kafka, there is maybe something wrong: %s", strOut)
+  return fmt.Errorf("Unable to parse results from kafka, there is maybe something wrong: %s", strOut)
 }
 
 func (client *KafkaManagingClient) describeTopic(name string) (*KafkaTopicInfo, error) {
@@ -100,10 +132,10 @@ func readError(txt string) error {
 }
 
 func readTopicInfo(txt string) (*KafkaTopicInfo, error) {
-  partsR, _ := regexp.Compile("PartitionCount:(\\d+).+ReplicationFactor:(\\d+)")
+  partsR, _ := regexp.Compile("PartitionCount:(\\d+).+ReplicationFactor:(\\d+).+Configs:\\s*([^\\s]+)?")
   pRes := partsR.FindStringSubmatch(txt)
-  if len(pRes) != 3 {
-    return nil, fmt.Errorf("Unable to determine topic's partitions count")
+  if len(pRes) != 4 {
+    return nil, fmt.Errorf("Unable to determine topic's partitions count (Unexpected format)")
   }
 
   pCount, pcErr := strconv.Atoi(pRes[1])
@@ -116,12 +148,35 @@ func readTopicInfo(txt string) (*KafkaTopicInfo, error) {
     return nil, fmt.Errorf("Unable to read topic's partition count: " + rErr.Error())
   }
 
+  // Read config options
+  confOpts := make(map[string]string)
+
+  for _, e := range strings.Split(pRes[3], ",") {
+    if !strings.Contains(e, "=") { continue }
+    ps := strings.SplitN(e, "=", 2)
+    confOpts[ps[0]] = ps[1]
+  }
+
   info := &KafkaTopicInfo{
-    PartitionsCount: pCount,
+    PartitionsCount:   pCount,
     ReplicationFactor: rCount,
+    CleanupPolicy:     getOrDefaultStr(confOpts, "cleanup.policy", ""),
+    RetentionBytes:    getOrDefaultInt(confOpts, "retention.bytes", -1),
+    RetentionMs:       getOrDefaultInt(confOpts, "retention.ms", -1),
   }
 
   return info, nil
 }
 
+func getOrDefaultStr(m map[string]string, key string, def string) string {
+  if v, ok := m[key]; ok { return v }
+  return def
+}
+
+func getOrDefaultInt(m map[string]string, key string, def int64) int64 {
+  if v, ok := m[key]; ok {
+    if i, pok := strconv.ParseInt(v, 10, 0); pok == nil { return i }
+  }
+  return def
+}
 
